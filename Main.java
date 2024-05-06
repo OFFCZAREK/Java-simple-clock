c8db077595ca534c4493354d2f8936e4327de9f0
OFFCZAREK <169077809+OFFCZAREK@users.noreply.github.com>
2024-05-06 19:28:55 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public void setVisaible(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVisaible'");
+    }
+
+    public void setVisible(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVisible'");
+    }
+
+}
