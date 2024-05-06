5a9c1ff3110831d3c76256089f2813e1e74dde6c
OFFCZAREK <169077809+OFFCZAREK@users.noreply.github.com>
2024-05-06 19:30:11 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-public class Main {
-
-    public void setVisaible(boolean b) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setVisaible'");
+public class Main extends JFrame
+{
+   public Main()
+   {
+   this.setBounds(300 ,300 ,300 ,200);
+   initComponents();
+    this.setDefaultCloseOperation(3);
+   }
+  public void initComponents()
+  {
+      panel.add(label);
+      panel.add(Time);
+      
+      ActionListener stoper = new Clock();
+      
+      Timer clock = new Timer(1000, stoper );
+      
+      clock.start();
+      
+      this.getContentPane().add(panel);
+  }  
+  private class Clock implements ActionListener
+  {
+     public void actionPerformed(ActionEvent e)
+     {
+         Time.setText(downloadTime());
+     }
+  }
+    
+  JPanel panel =new JPanel();
+  JLabel label =new JLabel("Time : ");
+  JLabel Time = new JLabel(downloadTime());
+    
+  public String downloadTime()
+  {
+      GregorianCalendar kalendar = new GregorianCalendar();
+         
+         String h = ""+kalendar.get(Calendar.HOUR_OF_DAY);
+         String min = ""+kalendar.get(Calendar.MINUTE);
+         String sec = ""+kalendar.get(Calendar.SECOND);
+         
+         if(Integer.parseInt(h) <10)
+             h= "0"+h;
+         if(Integer.parseInt(min) <10)
+             min= "0"+min;
+         if(Integer.parseInt(sec) <10)
+             sec= "0"+sec;
+      return h+" : "+min+" : "+sec;
+  }
+    
+public static void main(String[] args) 
+    {
+        new Main().setVisible(true);
     }
-
-    public void setVisible(boolean b) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setVisible'");
-    }
-
+ 
 }
